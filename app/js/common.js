4328f5da10e53450b6ac251a5e959df7edc4cafc
Hastle <alexandrs.me.com@gmail.com>
2023-07-05 23:58:39 +0300

    styles for infobar, coutDown timer

--- app/js/common.js
+++ app/js/common.js
@@ -1,20 +1,5 @@
 const actionArea = document.getElementById("action-area");
 const numberTask = document.getElementById("number-task");
-
-for (let i = 0; i < 6; i++) {
-	const button = document.createElement("button");
-	button.classList.add("number");
-	actionArea.appendChild(button);
-
-	button.addEventListener("click", () => {
-		if (button.textContent == numberTask.textContent)
-			console.log("Верно");
-		else
-			console.log("Неверно");
-	});
-}
-
-const numberButtons = document.querySelectorAll('.number');
 const colors = [
 	'#FF4136',
 	'#FF851B',
@@ -26,13 +11,42 @@ const colors = [
 	'#FF0080',
 	'#F012BE',
 	'#85144b' 
- ];
+];
+
+for (let i = 0; i < 6; i++) {
+	const button = document.createElement("button");
+	button.classList.add("number");
+	actionArea.appendChild(button);
+}
+
+const numberButtons = document.querySelectorAll('.number');
+
+
+function setNumbers() {
+	numberButtons.forEach(button => {
+		const randomNumber = Math.floor(Math.random() * 100);
+		button.textContent = randomNumber;
+		const randomColorIndex = Math.floor(Math.random() * colors.length);
+		button.style.backgroundColor = colors[randomColorIndex];
+	});
+	const randomButtonIndex = Math.floor(Math.random() * numberButtons.length);
+	const randomButton = numberButtons[randomButtonIndex];
+	numberTask.textContent = randomButton.textContent;
+}
+
+setNumbers();
+
+
 numberButtons.forEach(button => {
-	const randomNumber = Math.floor(Math.random() * 100);
-	button.textContent = randomNumber;
-	const randomColorIndex = Math.floor(Math.random() * colors.length);
-	button.style.backgroundColor = colors[randomColorIndex];
-	numberTask.textContent = randomNumber;
+	button.addEventListener("click", () => {
+		if (button.textContent === numberTask.textContent) {
+			console.log("Верно");
+		}
+		else {
+			console.log("Неверно");
+		}
+		setNumbers();
+	});
 });
 
 
